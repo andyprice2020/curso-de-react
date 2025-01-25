f3ba463e207a776475b6d4b969bad025b9da0ab0
Andres Grover Albino Chambi <grover.albino@gmail.com>
2025-01-25 18:02:06 -0400

    Manejo de Rutas con react-router, manejo de Hooks de manera inicial, clase 4

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,9 +1,18 @@
 import { useSelector } from 'react-redux'
+import { useState } from 'react'
 const About = () => {
     const initialValue = useSelector((state) => state.product.initialValue)
+    /* Manejo de Hooks
+    Los Hooks son funciones ya definidas en React que nos permiten 
+    usar el estado y el ciclo de vida en componentes funcionales.
+    Es posible crear Hooks personalizados 
+    El Hook useState permite manejar variables de Estado*/
+    const [ nameComponent, setNameComponent ] = useState('About')
     return(
         <>
         <h2>Initial value is: {initialValue}</h2>
+        <p>Component name: {nameComponent} </p>
+        <button onClick={() => {setNameComponent('Think')}}>Change Name</button>
         </>
     );
 }
